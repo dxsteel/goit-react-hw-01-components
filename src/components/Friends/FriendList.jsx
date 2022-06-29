a22b2d73fe9ca0be2f2838033ff2f41346491da2
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-06-29 20:44:55 +0300

    Download components
    file json;
    friendlist;
    statistics;
    transactions;
    social profile

---
+++ src/components/Friends/FriendList.jsx
@@ -0,0 +1,31 @@
+import styles from './FriendList.module.css'
+
+export function FriendsListItem({ avatar, name, isOnline }) {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.online : styles.offline}></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt="User avatar"
+        width="300"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+}
+
+export function FriendList({ friends }) {
+    return (
+<ul className={StyleSheet.friendList}>
+{friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendsListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+</ul>
+    )
+}
