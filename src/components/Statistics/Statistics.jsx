73d30eeb05a5a85bfbeb63e1a5a831d1f51e4d4b
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-07-01 02:24:39 +0300

    download props and css modules

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,9 @@
+import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
 export function Statistics({ title, stats }) {
-    return (
+  return (
+      <container className={styles.container}>
        <section className={styles.statistics}>
       {title && <h2 className={styles.title}>{title}</h2>}
 
@@ -12,12 +14,13 @@ export function Statistics({ title, stats }) {
             className={styles.item}
             style={{ backgroundColor: getRandomHexColor() }}
           >
-            <span className={label}>{label}</span>
-            <span className={percentage}>{percentage}%</span>
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
-    </section>
+      </section>
+      </container>
     );
 
     
@@ -25,3 +28,16 @@ export function Statistics({ title, stats }) {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      }
+    )
+  ),
+};
--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -1,7 +1,8 @@
-
+import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css'
 export function TransactionHistory({ items }) {
     return (
-<table className={TransactionHistory}>
+<table className={styles.transactionHistory}>
   <thead>
     <tr>
       <th>Type</th>
@@ -13,12 +14,27 @@ export function TransactionHistory({ items }) {
   <tbody>
     {items.map(({ type, amount, currency, id }) => (
           <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+            <td style={{ color: getRandomHexColor() }}>{type}</td>
+            <td style={{ color: getRandomHexColor() }}>{amount}</td>
+            <td style={{ color: getRandomHexColor() }}>{currency}</td>
           </tr>
         ))}
   </tbody>
 </table>
     )
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
